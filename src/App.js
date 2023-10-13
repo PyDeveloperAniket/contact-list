1a8fca65ef636ad6becff4d2dd2229ecbb2e536b
Aniket Wandile <63203988+PyDeveloperAniket@users.noreply.github.com>
2023-10-14 01:57:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,13 +9,20 @@ import EditContact from "./components/EditContact";
 import { useDispatch } from "react-redux";
 
 const App = () => {
+    // Redux dispatch hook
     const dispatch = useDispatch();
+
+    // Fetch data from an external API when the component mounts
     useEffect(() => {
+        // Initialize an empty array to store fetched data
         const data = [];
-        const promise = async () => {
+
+        // Asynchronous function to fetch data from the API
+        const fetchData = async () => {
             await fetch('https://jsonplaceholder.typicode.com/users')
                 .then((response) => response.json())
                 .then((json) => {
+                    // Map over the fetched data and transform it into the desired format
                     json.map((contact) => {
                         data.push({
                             id: contact.id,
@@ -25,26 +32,34 @@ const App = () => {
                         });
                     })
                 });
+
+            // Dispatch an action to update the Redux store with the fetched data
             dispatch({ type: 'FETCH_CONTACTS', payload: data });
         };
-        promise();
-    }, []);
 
+        // Call the fetchData function when the component mounts
+        fetchData();
+    }, [dispatch]);
 
+    // JSX structure for the App component
     return (
         <div className="App">
+            {/* ToastContainer for displaying notifications */}
             <ToastContainer />
+
+            {/* Navbar component */}
             <Navbar />
+
+            {/* React Router's Routes component to handle navigation */}
             <Routes>
-                <Route exact path="/" element={<Home />}>
+                {/* Route for the home page */}
+                <Route exact path="/" element={<Home />} />
 
-                </Route>
-                <Route path="/add" element={<AddContact />}>
+                {/* Route for adding a new contact */}
+                <Route path="/add" element={<AddContact />} />
 
-                </Route>
-                <Route path="/edit/:id" element={<EditContact />}>
-
-                </Route>
+                {/* Route for editing an existing contact */}
+                <Route path="/edit/:id" element={<EditContact />} />
             </Routes>
         </div>
     );
