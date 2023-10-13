937121b51334a7a3be49d7de8e517e90bd4ee199
Aniket Wandile <63203988+PyDeveloperAniket@users.noreply.github.com>
2023-10-14 01:55:36 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
+        // Bootstrap Navbar component
         <nav className='navbar navbar-expand-lg navbar-light ml-3'>
             <div className='container-fluid'>
+                {/* Link to the home page */}
                 <Link to='/' className='navbar-brand'>Contact List App</Link>
             </div>
         </nav >
     )
 }
 
-export default Navbar
+export default Navbar;
