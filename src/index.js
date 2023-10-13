dd153d30219dfded8c03e8d3e2435204fb525d2a
Aniket Wandile <63203988+PyDeveloperAniket@users.noreply.github.com>
2023-10-14 01:57:58 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,18 +2,28 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
+// Importing Bootstrap CSS for styling
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Importing React Toastify styles for notifications
 import 'react-toastify/dist/ReactToastify.css';
+
+// Importing BrowserRouter for routing
 import { BrowserRouter as Router } from 'react-router-dom';
+
+// Importing Redux related modules
 import { createStore } from 'redux';
 import contactReducer from './redux/reducers/contactReducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
 
+// Creating the Redux store with the contactReducer and Redux DevTools extension
 const store = createStore(contactReducer, composeWithDevTools());
 
+// Creating a root for rendering the React app
 const root = createRoot(document.querySelector('#root'));
 
+// Rendering the app within the Redux Provider and Router
 root.render(
     <Provider store={store}>
         <Router>
